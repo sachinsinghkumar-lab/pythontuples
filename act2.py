6f99e29c205440beebbde0608b4d190c8e1236dd
sachinsinghkumar-lab <sachinsinghkumar@gmail.com>
2026-03-22 19:22:53 +0530

    Update

---
+++ act2.py
@@ -0,0 +1,18 @@
+def palindrome(r):
+    e = len(r)-1
+    s = 0
+    while(s>e):
+        if(r[s]!=r[e]):
+            return False
+        s+=1
+        e-=1
+    return True
+
+r = (2,1)
+
+if(palindrome(r)):
+    print("the tuple is flip flop")
+else:
+    print("the tuple is not flip flop!!!")
+
+
